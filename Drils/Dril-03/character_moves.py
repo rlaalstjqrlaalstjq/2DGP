7d59dbffe871d2eb2d386829017e005ca8ae3c08
minsubkim <kpukms21@naver.com>
2018-09-10 21:33:45 +0900

    Drill 03 완성
    2014180007 김민섭
    https://youtu.be/KeUi-veNPdk

--- Drils/Dril-03/character_moves.py
+++ Drils/Dril-03/character_moves.py
@@ -1,18 +1,67 @@
 from pico2d import *
 
+import math
+
+os.chdir('C:\\Users\김민섭\\Desktop\\2D 프로그래밍\\2018-2DGP\\Labs\\Lecture03')
+
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
 x= 0
+y=0
+i=0
+angle=5
+a= (math.radians(angle))
 
-while( x <800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x = x+2
-    delay(0.01)
+while True:
+        while( x <800):
+                clear_canvas_now()
+                grass.draw_now(400,30)
+                character.draw_now(x,90)
+                x = x+4
+                delay(0.01)
+        while( y <570):
+                clear_canvas_now()
+                grass.draw_now(400,30)
+                character.draw_now(790,y+90)
+                y = y+4
+                delay(0.01)
+        while( x >0):
+                clear_canvas_now()
+                grass.draw_now(400,30)
+                character.draw_now(x,560)
+                x = x-4
+                delay(0.01)   
+        while( y > 0):
+                clear_canvas_now()
+                grass.draw_now(400,30)
+                character.draw_now(0,y+90)
+                y = y-4
+                delay(0.01)
+        while( x <400):
+                clear_canvas_now()
+                grass.draw_now(400,30)
+                character.draw_now(x,90)
+                x = x+4
+                delay(0.01)
+        for k in range(0,71):
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            x= 400 +210*(math.cos(a*(120-k)))
+            y= 300 + 210*(math.sin(a*(120-k)))
+            
+            delay(0.04)
     
     
+    
+
+        
 close_canvas()
+
+
+
+    
+
