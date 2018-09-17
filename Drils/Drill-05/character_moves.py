29c66c3f94b2bfe1f4bbc281d4859971bed7b4a2
minsubkim <kpukms21@naver.com>
2018-09-17 15:00:03 +0900

    step 3
    2014180007 김민섭
    좌표 2번째 이동

--- Drils/Drill-05/character_moves.py
+++ Drils/Drill-05/character_moves.py
@@ -23,7 +23,7 @@ def move_point1():
     while y < 535:
         clear_canvas()
         grass.draw(400, 30)
-        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y+30)
 
         update_canvas()
         frame = (frame + 1) % 8
@@ -33,7 +33,29 @@ def move_point1():
 
 
 def move_point2():
-    pass
+    x, y = 203, 535
+    frame = 0
+    while x > 132:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.02)
+        get_events()
+
+    while y > 243:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+
+        update_canvas()
+        frame = (frame + 1) % 8
+        y -= 5
+        delay(0.02)
+        get_events()
 
 
 def move_point3():
@@ -69,7 +91,7 @@ def move_point10():
 
 
 while True:
-    move_point1()
+    #move_point1()
     move_point2()
     move_point3()
     move_point4()
