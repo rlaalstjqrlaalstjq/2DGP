508d097e5bd5752b46b1a566d2c90e8efd1fec07
minsubkim <kpukms21@naver.com>
2018-09-17 14:52:33 +0900

    step1
    2014180007 김민섭

---
+++ Drils/Drill-05/character_moves.py
@@ -0,0 +1,61 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+
+def move_point1():
+    pass
+
+
+def move_point2():
+    pass
+
+
+def move_point3():
+    pass
+
+
+def move_point4():
+    pass
+
+
+def move_point5():
+    pass
+
+
+def move_point6():
+    pass
+
+
+def move_point7():
+    pass
+
+
+def move_point8():
+    pass
+
+
+def move_point9():
+    pass
+
+
+def move_point10():
+    pass
+
+
+while True:
+    move_point1()
+    move_point2()
+    move_point3()
+    move_point4()
+    move_point5()
+    move_point6()
+    move_point7()
+    move_point8()
+    move_point9()
+    move_point10()
+
+close_canvas()
