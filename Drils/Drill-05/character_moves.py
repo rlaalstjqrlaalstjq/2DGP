31a415259ad0028ea27cbf763f87fd01ab1eeacc
minsubkim <kpukms21@naver.com>
2018-09-17 15:25:00 +0900

    step14 최종본
    14번 step 최종점검.
    
    2014180007 김민섭

--- Drils/Drill-05/character_moves.py
+++ Drils/Drill-05/character_moves.py
@@ -269,14 +269,14 @@ def move_point10():
 move_point1()
 
 while True:
-    move_point2()
-    move_point3()
-    move_point4()
-    move_point5()
-    move_point6()
-    move_point7()
-    move_point8()
-    move_point9()
-    move_point10()
+    move_run_point2()
+    move_run_point3()
+    move_run_point4()
+    move_run_point5()
+    move_run_point6()
+    move_run_point7()
+    move_run_point8()
+    move_run_point9()
+    move_run_point10()
 
 close_canvas()
