b068e1063baa4ff2540797b24d2d05bafda1e8e0
minsubkim <kpukms21@naver.com>
2018-10-08 14:41:01 +0900

    step 3
    3선완성

--- Drils/Drill-08/curve.py
+++ Drils/Drill-08/curve.py
@@ -92,7 +92,12 @@ def draw_curve_5_points(p1, p2, p3, p4,p5):
     draw_point(p3)
 
     # draw p3-p4
-    pass
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p2[0] + (3*t**3 - 5*t**2 + 2)*p3[0] + (-3*t**3 + 4*t**2 + t)*p4[0] + (t**3 - t**2)*p5[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p2[1] + (3*t**3 - 5*t**2 + 2)*p3[1] + (-3*t**3 + 4*t**2 + t)*p4[1] + (t**3 - t**2)*p5[1])/2
+        draw_point((x, y))
+    draw_point(p4)
 
     # draw p4-p5
     pass
